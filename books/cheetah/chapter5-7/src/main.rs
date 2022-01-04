352553c13933c2ef2fed2ccbc00bb83f8ffc1072
asakuchi <paleApricot.asakuchi@gmail.com>
2022-01-04 21:00:43 +0900

    enumで表現、unwrapを減らす

--- books/cheetah/chapter5-7/src/main.rs
+++ books/cheetah/chapter5-7/src/main.rs
@@ -2,6 +2,15 @@
 
 use proconio::input;
 
+#[derive(Copy, Clone, Debug)]
+enum Cell {
+    /// 未踏
+    Unexplored,
+    Step(usize),
+    /// 通行不可
+    Unavailable,
+}
+
 fn main() {
     input! {
         n: usize, // maze の要素数
@@ -22,48 +31,61 @@ fn main() {
     let width = maze[0].len();
     let height = n;
 
-    let mut steps = vec![vec![-1; width]; height];
-    steps[start_row][start_col] = 0;
-
+    let mut steps = vec![vec![Cell::Unexplored; width]; height];
+    steps[start_row][start_col] = Cell::Step(0);
     // println!("steps:{:?}", steps);
+    let mut queue = std::collections::VecDeque::<(usize, usize)>::new();
+    queue.push_back((start_row, start_col));
 
-    let mut queue = std::collections::VecDeque::<(isize, isize)>::new();
-    queue.push_back((start_row as isize, start_col as isize));
-
-    while queue.len() > 0 {
-        let target = queue.pop_front().unwrap();
-
+    while let Some(target) = queue.pop_front() {
         for pattern in move_row.iter().zip(move_col.iter()) {
-            let next = (target.0 + pattern.0, target.1 + pattern.1);
+            let next = (target.0 as isize + pattern.0, target.1 as isize + pattern.1);
 
-            if 0 <= next.0
-                && next.0 < height as isize
-                && 0 <= next.1
-                && next.1 < width as isize
-                && steps[next.0 as usize][next.1 as usize] == -1
-                && maze[next.0 as usize].chars().nth(next.1 as usize).unwrap() != 'X'
-            {
-                steps[next.0 as usize][next.1 as usize] =
-                    steps[target.0 as usize][target.1 as usize] + 1;
-                queue.push_back(next);
+            if 0 > next.0 || next.0 >= height as isize || 0 > next.1 || next.1 >= width as isize {
+                continue;
+            }
+
+            let next = (next.0 as usize, next.1 as usize);
+
+            if maze[next.0].chars().nth(next.1).unwrap() == 'X' {
+                steps[next.0][next.1] = Cell::Unavailable;
+                continue;
+            }
+
+            if let Cell::Unexplored = steps[next.0][next.1] {
+                if let Cell::Step(step) = steps[target.0][target.1] {
+                    steps[next.0][next.1] = Cell::Step(step + 1);
+                    queue.push_back(next);
+                }
             }
         }
     }
 
     let mut result = 0;
+    let mut is_unreachable = false;
 
     // println!("steps:{:?}", steps);
 
-    'outer: for (i, line) in steps.iter().enumerate() {
-        for (j, &value) in line.iter().enumerate() {
-            if maze[i].chars().nth(j).unwrap() != 'X' && value == -1 {
-                result = -1;
-                break 'outer;
+    'outer: for line in steps.iter() {
+        for &value in line.iter() {
+            match value {
+                Cell::Unexplored => {
+                    is_unreachable = true;
+                    break 'outer;
+                }
+                Cell::Step(step) => {
+                    result = std::cmp::max(step, result);
+                }
+                Cell::Unavailable => {
+                    // do nothing.
+                }
             }
-
-            result = std::cmp::max(value, result);
         }
     }
 
-    println!("{}", result);
+    if is_unreachable {
+        println!("{}", -1);
+    } else {
+        println!("{}", result);
+    }
 }
