1293a3699d732eb479bd8609977738386af75a01
asakuchi <paleApricot.asakuchi@gmail.com>
2022-01-05 03:16:40 +0900

    間違ったコミットの戻し

--- books/cheetah/chapter3/src/main.rs
+++ books/cheetah/chapter3/src/main.rs
@@ -3,22 +3,101 @@
 // use proconio::input;
 
 fn main() {
-    let list = [[1, 2, 3], [4, 5, 6]];
-    // assert_eq!(hash.values().max(), Some(&2));
+    // if, else
+    println!("{}", is_even(10));
+    println!("{}", is_even(11));
 
-    for line in list.iter() {
-        for value in line {
-            println!("{}", value);
+    // for
+    println!("{}", get_range_sum(10));
+
+    // 配列
+    println!("{}", get_max_num(vec![1, 20, 3]));
+
+    // ソート
+    let mut numbers = vec![3, 4, 7, 2, 5, 6, 1];
+    numbers.sort();
+    println!("{:?}", numbers);
+
+    // 文字列処理
+    let s = "abc";
+
+    // 同値判定
+    if s == "abc" {
+        println!("equal");
+    }
+
+    // 1文字の切り出し
+    let c = s.chars().nth(0).unwrap();
+    println!("{}", c);
+
+    // 文字の連結
+    let s = format!("{}{}{}", "def", s, "ghi");
+    println!("{}", s);
+
+    // 文字の切り出し
+    let s = &s[3..6];
+    println!("{}", s);
+
+    // 連想配列
+    count_strings(vec!["hello", "world", "hello"]);
+}
+
+///
+/// if,else
+/// Rust ではif文ではなくif式
+///
+fn is_even(a: i32) -> i32 {
+    if a % 2 == 0 {
+        1
+    } else {
+        0
+    }
+}
+
+///
+/// for
+///
+fn get_range_sum(n: i32) -> i32 {
+    let mut sum = 0;
+
+    // 1..n で n を含まない
+    // 1..=n で n を含む
+    for i in 1..=n {
+        sum += i;
+    }
+
+    sum
+}
+
+///
+/// 配列
+/// （↓はVecだが・・・）
+///
+fn get_max_num(array: Vec<i32>) -> i32 {
+    let mut ret = array[0];
+
+    for value in array {
+        if ret < value {
+            ret = value;
         }
     }
 
-    let list = [1, 2, 3];
+    ret
+}
 
-    for value in list.iter() {
-        println!("{}", value);
+///
+/// 連想配列
+///
+fn count_strings(s: Vec<&str>) {
+    // 連想配列
+
+    let mut hm = std::collections::HashMap::new();
+
+    for text in s {
+        *hm.entry(text).or_insert(0) += 1;
     }
 
-    println!("{:?}", list);
-
-    println!("end.");
+    for (key, value) in hm {
+        println!("{} {}", key, value);
+    }
 }
