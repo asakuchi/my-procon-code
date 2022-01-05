a3a4206c72334032d6c17f3b801e660c3d9de44e
asakuchi <paleApricot.asakuchi@gmail.com>
2022-01-06 01:04:53 +0900

    メモ化

--- books/cheetah/chapter7-4/src/main.rs
+++ books/cheetah/chapter7-4/src/main.rs
@@ -11,36 +11,64 @@ fn main() {
         donations: [usize; n],
     }
 
-    let max = dfs(&donations, 0, false, false);
+    let mut memo = vec![vec![vec![-1; 2]; 2]; n];
+
+    let max = dfs(&donations, 0, false, false, &mut memo);
 
     println!("{}", max);
 }
 
-fn dfs(donations: &Vec<usize>, index: usize, previous_donated: bool, first_donated: bool) -> usize {
+fn dfs(
+    donations: &Vec<usize>,
+    index: usize,
+    previous_donated: bool,
+    first_donated: bool,
+    memo: &mut Vec<Vec<Vec<i32>>>,
+) -> usize {
     if index == donations.len() {
         return 0;
     }
 
+    if memo[index][if previous_donated { 1 } else { 0 }][if first_donated { 1 } else { 0 }] != -1 {
+        return memo[index][if previous_donated { 1 } else { 0 }][if first_donated { 1 } else { 0 }]
+            as usize;
+    }
+
     if previous_donated {
         // 隣人が寄付したらこの住人は寄付しない
-        return dfs(donations, index + 1, false, first_donated);
+        let value = dfs(donations, index + 1, false, first_donated, memo);
+
+        memo[index][if previous_donated { 1 } else { 0 }][if first_donated { 1 } else { 0 }] =
+            value as i32;
+
+        return value;
     }
 
     if index == donations.len() - 1 && first_donated {
         // 最初の住人が寄付したら最後の住人は寄付しない
         // （最初の住人と最後の住人は隣人）
-        return dfs(donations, index + 1, false, first_donated);
+        let value = dfs(donations, index + 1, false, first_donated, memo);
+
+        memo[index][if previous_donated { 1 } else { 0 }][if first_donated { 1 } else { 0 }] =
+            value as i32;
+
+        return value;
     }
 
     let selected = if index == 0 {
         // 最初の住人が寄付したかどうかを記録
         // 最後の住人の寄付でチェック
-        dfs(donations, index + 1, true, true) + donations[index]
+        dfs(donations, index + 1, true, true, memo) + donations[index]
     } else {
-        dfs(donations, index + 1, true, first_donated) + donations[index]
+        dfs(donations, index + 1, true, first_donated, memo) + donations[index]
     };
 
-    let unselected = dfs(donations, index + 1, false, first_donated);
+    let unselected = dfs(donations, index + 1, false, first_donated, memo);
 
-    std::cmp::max(selected, unselected)
+    let value = std::cmp::max(selected, unselected);
+
+    memo[index][if previous_donated { 1 } else { 0 }][if first_donated { 1 } else { 0 }] =
+        value as i32;
+
+    value
 }
