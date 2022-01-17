798557bd1ba0210679ba87e03e0ad366be192445
asakuchi <paleApricot.asakuchi@gmail.com>
2022-01-18 01:51:54 +0900

    見栄え

--- books/ant/3-4/src/main.rs
+++ books/ant/3-4/src/main.rs
@@ -25,29 +25,16 @@ fn main() {
     }
 
     let mut dp = vec![vec![-1; MAX_N]; 1 << MAX_N];
-    // let dp = [[0; MAX_N]; 1 << MAX_N];
 
     let result = rec(0, 0, n, &d, &mut dp);
-
-    // println!("{:?}", dp);
 
     println!("{}", result);
 }
 
 fn rec(visited: usize, current: usize, n: usize, d: &Vec<Vec<i32>>, dp: &mut Vec<Vec<i32>>) -> i32 {
-    // println!("{:?}", visited);
-
     if dp[visited][current] >= 0 {
         return dp[visited][current];
     }
-
-    // println!(
-    //     "探索終了？:{} {} {} {}",
-    //     visited,
-    //     (1 << n),
-    //     (1 << n) - 1,
-    //     current
-    // );
 
     if visited == (1 << n) - 1 && current == 0 {
         // 探索完了
@@ -58,12 +45,10 @@ fn rec(visited: usize, current: usize, n: usize, d: &Vec<Vec<i32>>, dp: &mut Vec
     let mut result = INF;
 
     for u in 0..n {
-        // println!("next :{} {} {}", visited, visited >> u, visited >> u & 1);
+        if visited & 1 << u == 0 {
+            let u_score = rec(visited | 1 << u, u, n, d, dp) + d[current][u];
 
-        if (visited >> u & 1) != 1 {
-            // println!("going next :{} {}", 1 << u, visited | 1 << u);
-
-            result = std::cmp::min(result, rec(visited | 1 << u, u, n, d, dp) + d[current][u]);
+            result = std::cmp::min(result, u_score);
         }
     }
 
