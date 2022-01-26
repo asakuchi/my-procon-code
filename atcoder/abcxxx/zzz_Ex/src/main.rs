477611a716e99fed8b811f0c4aa9c83e2b4d227d
asakuchi <paleApricot.asakuchi@gmail.com>
2022-01-26 20:41:57 +0900

    テンプレにfastout追加

--- atcoder/abcxxx/zzz_Ex/src/main.rs
+++ atcoder/abcxxx/zzz_Ex/src/main.rs
@@ -1,11 +1,13 @@
 // -*- coding:utf-8-unix -*-
 
+use proconio::fastout;
 use proconio::input;
 // use proconio::derive_readable;
 // use proconio::marker::Chars;
 // use itertools::izip;
 // use itertools::Itertools;
 
+#[fastout]
 fn main() {
     input! {
         n: usize,
