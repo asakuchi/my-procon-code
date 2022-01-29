3c3461eaf954d5dd7a65b2ca210c1fbc871023e3
asakuchi <paleApricot.asakuchi@gmail.com>
2022-01-30 01:25:04 +0900

    計算する必要がない部分を省略

--- atcoder/abc133/C/src/main.rs
+++ atcoder/abc133/C/src/main.rs
@@ -23,16 +23,6 @@ fn main() {
 
         println!("{}", min);
     } else {
-        let mut min = 2020;
-
-        for i in 0..2019 {
-            for j in i + 1..=2019 {
-                let num = (i * j) % 2019;
-
-                min = std::cmp::min(min, num);
-            }
-        }
-
-        println!("{}", min);
+        println!("0");
     }
 }
