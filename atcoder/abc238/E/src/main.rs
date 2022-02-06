6434aa5ce5b3bbbb3b67cf94bf828a8871c60dcc
asakuchi <paleApricot.asakuchi@gmail.com>
2022-02-06 15:01:53 +0900

    ABC238 E

--- atcoder/abc238/E/src/main.rs
+++ atcoder/abc238/E/src/main.rs
@@ -1,24 +1,84 @@
-// -*- coding:utf-8-unix -*-
-
 use proconio::fastout;
 use proconio::input;
-// use proconio::derive_readable;
-// use proconio::marker::Chars;
-// use itertools::izip;
-// use itertools::Itertools;
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        m: usize,
-        a: [usize; n],
-        p: [[usize; m]; n],
-        mut plan: [(usize, usize); n],
-        s: String,
-
+        q: usize,
+        lr: [(usize, usize); q],
     }
 
-    // println!("{}", if yes { "Yes" } else { "No" });
-    // println!("{}", yes);
+    let mut set = DisjointSet::new_with((n + 1) as i64);
+
+    for (l, r) in &lr {
+        let l = l - 1;
+
+        set.unite(l as i64, *r as i64);
+    }
+
+    if set.same(0, n as i64) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
+#[derive(Debug)]
+pub struct DisjointSet {
+    rank: Vec<i64>,
+    p: Vec<i64>,
+}
+
+impl DisjointSet {
+    pub fn new() -> DisjointSet {
+        DisjointSet {
+            rank: vec![-1; 1_000_000],
+            p: vec![-1; 1_000_000],
+        }
+    }
+
+    pub fn new_with(size: i64) -> DisjointSet {
+        let mut set = DisjointSet {
+            rank: vec![-1; 1_000_000],
+            p: vec![-1; 1_000_000],
+        };
+
+        for i in 0..size {
+            set.make_set(i);
+        }
+
+        set
+    }
+
+    pub fn make_set(&mut self, x: i64) {
+        self.p[x as usize] = x;
+        self.rank[x as usize] = 0;
+    }
+
+    pub fn find_set(&mut self, x: i64) -> i64 {
+        if x != self.p[x as usize] {
+            self.p[x as usize] = self.find_set(self.p[x as usize]);
+        }
+
+        self.p[x as usize]
+    }
+
+    pub fn unite(&mut self, x: i64, y: i64) {
+        let a = self.find_set(self.p[x as usize]);
+        let b = self.find_set(self.p[y as usize]);
+
+        if self.rank[a as usize] > self.rank[b as usize] {
+            self.p[b as usize] = a;
+        } else {
+            self.p[a as usize] = b;
+            if self.rank[a as usize] == self.rank[b as usize] {
+                self.rank[b as usize] += 1;
+            }
+        }
+    }
+
+    pub fn same(&mut self, x: i64, y: i64) -> bool {
+        self.find_set(x) == self.find_set(y)
+    }
 }
