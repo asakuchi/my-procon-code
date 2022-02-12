53a069e37dca612e3efe57422e880f2e66dd18b4
asakuchi <paleApricot.asakuchi@gmail.com>
2022-02-12 16:01:48 +0900

    DPL_1_A 漸化式

--- aoj/DPL_1/A/src/main.rs
+++ aoj/DPL_1/A/src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 
+const INF: usize = 100_000_000;
+
 fn main() {
     let stdin = io::stdin();
 
@@ -26,47 +28,15 @@ fn main() {
 
     /* --------------------------------------------------- */
 
-    // let mut dp = vec![vec![0; n + 1]; m + 1];
+    let mut dp = vec![INF; 50_000 + 10_000 + 1];
 
-    // for i in 0..m {
-    //     for j in 1..=n {
-    //         if vw[i].1 > j {
-    //             dp[i + 1][j] = dp[i][j];
-    //         } else {
-    //             dp[i + 1][j] = std::cmp::max(dp[i][j], dp[i + 1][j - vw[i].1] + vw[i].0);
-    //         }
-    //     }
-    // }
+    dp[0] = 0;
 
-    // println!("{}", dp[n][w]);
-
-    let mut dp = vec![vec![-1; n + 1]; m + 1];
-    let result = rec(n, m, &c, 0, n as isize, &mut dp);
-
-    println!("{}", result);
-}
-
-fn rec(n: usize, m: usize, c: &Vec<usize>, i: usize, j: isize, dp: &mut Vec<Vec<isize>>) -> usize {
-    if j == 0 {
-        return 0;
+    for i in 0..m {
+        for j in 0..=n {
+            dp[j + c[i]] = std::cmp::min(dp[j + c[i]], dp[j] + 1);
+        }
     }
 
-    if i >= m || j < 0 {
-        return 100_000_000;
-    }
-
-    if dp[i][j as usize] != -1 {
-        return dp[i][j as usize] as usize;
-    }
-
-    // 使わない
-    let score_1 = rec(n, m, c, i + 1, j, dp);
-    // 使う
-    let score_2 = rec(n, m, c, i, j - c[i] as isize, dp) + 1;
-
-    let score = std::cmp::min(score_1, score_2);
-
-    dp[i][j as usize] = score as isize;
-
-    score
+    println!("{}", dp[n]);
 }
