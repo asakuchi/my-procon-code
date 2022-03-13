a9bed1294b272d24972d1a431f0accd5ab4f93d8
asakuchi <paleApricot.asakuchi@gmail.com>
2022-03-14 02:22:20 +0900

    Vecで実装

--- atcoder/abc222/C/src/main.rs
+++ atcoder/abc222/C/src/main.rs
@@ -2,7 +2,6 @@ use proconio::fastout;
 use proconio::input;
 use proconio::marker::Chars;
 use std::cmp::Reverse;
-use std::collections::BTreeSet;
 
 #[fastout]
 fn main() {
@@ -12,77 +11,52 @@ fn main() {
         a: [Chars; n * 2],
     }
 
-    let mut rank = BTreeSet::new();
+    let mut rank = Vec::with_capacity(n * 2);
 
     for i in 0..2 * n {
         // 勝利数、番号
-        rank.insert((Reverse(0), i));
+        rank.push((0, i));
     }
 
     for round in 0..m {
-        let mut next_rank = BTreeSet::new();
+        let mut iteration = rank.iter_mut();
 
-        let mut iteration = rank.iter();
-
-        while iteration.len() > 0 {
-            let p1 = iteration.next().unwrap();
-            let p2 = iteration.next().unwrap();
-
-            let p1_next;
-            let p2_next;
+        for _ in 0..n {
+            let mut p1 = iteration.next().unwrap();
+            let mut p2 = iteration.next().unwrap();
 
             match a[p1.1][round] {
                 'G' => match a[p2.1][round] {
-                    'G' => {
-                        p1_next = (p1.0, p1.1);
-                        p2_next = (p2.0, p2.1);
-                    }
+                    'G' => {}
                     'C' => {
-                        p1_next = (Reverse((p1.0).0 + 1), p1.1);
-                        p2_next = (p2.0, p2.1);
+                        p1.0 += 1;
                     }
                     _ => {
-                        p1_next = (p1.0, p1.1);
-                        p2_next = (Reverse((p2.0).0 + 1), p2.1);
+                        p2.0 += 1;
                     }
                 },
                 'C' => match a[p2.1][round] {
                     'G' => {
-                        p1_next = (p1.0, p1.1);
-                        p2_next = (Reverse((p2.0).0 + 1), p2.1);
+                        p2.0 += 1;
                     }
-                    'C' => {
-                        p1_next = (p1.0, p1.1);
-                        p2_next = (p2.0, p2.1);
-                    }
+                    'C' => {}
                     _ => {
-                        p1_next = (Reverse((p1.0).0 + 1), p1.1);
-                        p2_next = (p2.0, p2.1);
+                        p1.0 += 1;
                     }
                 },
                 _ => match a[p2.1][round] {
                     'G' => {
-                        p1_next = (Reverse((p1.0).0 + 1), p1.1);
-                        p2_next = (p2.0, p2.1);
+                        p1.0 += 1;
                     }
                     'C' => {
-                        p1_next = (p1.0, p1.1);
-                        p2_next = (Reverse((p2.0).0 + 1), p2.1);
+                        p2.0 += 1;
                     }
-                    _ => {
-                        p1_next = (p1.0, p1.1);
-                        p2_next = (p2.0, p2.1);
-                    }
+                    _ => {}
                 },
             }
-
-            next_rank.insert(p1_next);
-            next_rank.insert(p2_next);
         }
 
-        rank = next_rank;
-
-        // println!("{:?}", rank);
+        rank.sort_by_key(|&(win, num)| (Reverse(win), num));
     }
 
     for &(_win, num) in rank.iter() {
