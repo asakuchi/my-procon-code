d5d095d705db9947beafe3dc1b1de97cab258f63
asakuchi <paleApricot.asakuchi@gmail.com>
2022-03-21 12:18:52 +0900

    ABC244 E メモ化再帰

--- atcoder/abc244/E/src/main.rs
+++ atcoder/abc244/E/src/main.rs
@@ -16,77 +16,55 @@ fn main() {
         mut uv: [(Usize1, Usize1); m],
     }
 
-    let mut matrix = vec![vec![false; n]; n];
+    let mut list = vec![vec![]; n];
 
     for &(u, v) in uv.iter() {
-        matrix[u][v] = true;
-        matrix[v][u] = true;
+        list[u].push(v);
+        list[v].push(u);
     }
 
-    let mut memo = vec![vec![vec![0; 2]; k + 1]; n];
+    let mut memo = vec![vec![vec![-1; 2]; k + 1]; n];
 
-    memo[s][0][0] = 1;
+    let result = rec(&list, n, m, t, x, s, k, true, &mut memo);
 
-    for i in 0..k {
-        for &(u, v) in uv.iter() {
-            for y in 0..2 {
-                memo[v][i + 1][if v == x { (!(y != 0)) as usize } else { y }] += memo[u][i][y];
-                memo[u][i + 1][if u == x { (!(y != 0)) as usize } else { y }] += memo[v][i][y];
+    println!("{}", result % MODULO);
+}
 
-                memo[v][i + 1][0] %= MODULO;
-                memo[v][i + 1][1] %= MODULO;
-                memo[u][i + 1][0] %= MODULO;
-                memo[u][i + 1][1] %= MODULO;
-            }
+fn rec(
+    list: &Vec<Vec<usize>>,
+    n: usize,
+    m: usize,
+    t: usize,
+    x: usize,
+    current: usize,
+    rest: usize,
+    is_x_even: bool,
+    memo: &mut Vec<Vec<Vec<isize>>>,
+) -> usize {
+    if rest == 0 {
+        if current == t && is_x_even {
+            return 1;
+        } else {
+            return 0;
         }
     }
 
-    println!("{}", memo[t][k][0]);
+    if memo[current][rest][is_x_even as usize] != -1 {
+        return memo[current][rest][is_x_even as usize] as usize;
+    }
 
-    // let mut memo = vec![vec![vec![-1; 2]; k + 1]; n];
+    let mut result = 0;
 
-    // let result = rec(&matrix, n, m, t, x, s, k, true, &mut memo);
+    let is_x_even_next = if current == x { !is_x_even } else { is_x_even };
 
-    // println!("{}", result % MODULO);
+    for &next in list[current].iter() {
+        result += rec(list, n, m, t, x, next, rest - 1, is_x_even_next, memo);
+        result %= MODULO;
+    }
+
+    result %= MODULO;
+
+    memo[current][rest][is_x_even as usize] = result as isize;
+
+    result
 }
-
-// fn rec(
-//     matrix: &Vec<Vec<bool>>,
-//     n: usize,
-//     m: usize,
-//     t: usize,
-//     x: usize,
-//     current: usize,
-//     rest: usize,
-//     is_x_even: bool,
-//     memo: &mut Vec<Vec<Vec<isize>>>,
-// ) -> usize {
-//     if rest == 0 {
-//         if current == t && is_x_even {
-//             return 1;
-//         } else {
-//             return 0;
-//         }
-//     }
-
-//     if memo[current][rest][is_x_even as usize] != -1 {
-//         return memo[current][rest][is_x_even as usize] as usize;
-//     }
-
-//     let mut result = 0;
-
-//     let is_x_even_next = if current == x { !is_x_even } else { is_x_even };
-
-//     for next in 0..n {
-//         if matrix[current][next] {
-//             result += rec(matrix, n, m, t, x, next, rest - 1, is_x_even_next, memo);
-//             result %= MODULO;
-//         }
-//     }
-
-//     result %= MODULO;
-
-//     memo[current][rest][is_x_even as usize] = result as isize;
-
-//     result
-// }
