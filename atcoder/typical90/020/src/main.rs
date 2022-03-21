7c186ba62d1c5c69def66a9d3bfdad59877622d4
asakuchi <paleApricot.asakuchi@gmail.com>
2022-03-21 18:18:51 +0900

    競プロ典型 90 問 020

---
+++ atcoder/typical90/020/src/main.rs
@@ -0,0 +1,13 @@
+use proconio::fastout;
+use proconio::input;
+
+#[fastout]
+fn main() {
+    input! {
+        a: u64,
+        b: u32,
+        c: u64,
+    }
+
+    println!("{}", if a < c.pow(b) { "Yes" } else { "No" });
+}
