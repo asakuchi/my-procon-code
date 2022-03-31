40fc2cb87256e80c46ff334847116cd0c419ad4e
asakuchi <paleApricot.asakuchi@gmail.com>
2022-03-31 23:16:01 +0900

    ABC130 D

---
+++ atcoder/abc130/D/src/main.rs
@@ -0,0 +1,37 @@
+use proconio::fastout;
+use proconio::input;
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize; n],
+    }
+
+    let mut result = 0;
+    let mut s = 0;
+    let mut t = 0;
+
+    let mut sum = a[0];
+
+    loop {
+        while sum < k && t < n - 1 {
+            t += 1;
+            sum += a[t];
+        }
+
+        while sum >= k {
+            result += n - t;
+
+            sum -= a[s];
+            s += 1;
+        }
+
+        if t == n - 1 {
+            break;
+        }
+    }
+
+    println!("{}", result);
+}
