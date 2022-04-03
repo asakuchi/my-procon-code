95d42c602d95ef097e3d0d4407fb5440f774c462
asakuchi <paleApricot.asakuchi@gmail.com>
2022-04-04 01:30:26 +0900

    ABC218 A

---
+++ atcoder/abc218/A/src/main.rs
@@ -0,0 +1,13 @@
+use proconio::fastout;
+use proconio::input;
+use proconio::marker::Chars;
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        s: Chars,
+    }
+
+    println!("{}", if s[n - 1] == 'o' { "Yes" } else { "No" });
+}
