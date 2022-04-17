651c0ffc9fdda55ed546cb1aafe2cbe54d0343b2
asakuchi <paleApricot.asakuchi@gmail.com>
2022-04-17 20:55:24 +0900

    ABC248 E 速度改善

--- atcoder/abc248/E/src/main.rs
+++ atcoder/abc248/E/src/main.rs
@@ -1,7 +1,7 @@
 use num_rational::Ratio;
 use proconio::fastout;
 use proconio::input;
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
@@ -16,8 +16,8 @@ fn main() {
         return;
     }
 
-    let mut result_normal = HashSet::new();
-    let mut result_x = HashSet::new();
+    let mut lines = HashMap::new();
+    let mut lines_x = HashMap::new();
 
     for i in 0..n {
         for j in i + 1..n {
@@ -25,47 +25,29 @@ fn main() {
             let (x2, y2) = xy[j];
 
             if x2 == x1 {
-                let mut count = 2;
-
-                for l in j + 1..n {
-                    if i == l || j == l {
-                        continue;
-                    }
-
-                    let (x3, _y3) = xy[l];
-
-                    if x3 == x1 {
-                        count += 1;
-                    }
-
-                    if count >= k {
-                        result_x.insert(x1);
-                    }
-                }
+                *lines_x.entry(x1).or_insert(0) += 1;
             } else {
-                let mut count = 2;
-
                 let a = Ratio::new_raw(y2 - y1, x2 - x1);
                 let b = Ratio::new_raw(y1, 1) - a * x1;
 
-                for l in j + 1..n {
-                    if i == l || j == l {
-                        continue;
-                    }
-
-                    let (x3, y3) = xy[l];
-
-                    if Ratio::new_raw(y3, 1) == a * x3 + b {
-                        count += 1;
-                    }
-                }
-
-                if count >= k {
-                    result_normal.insert((a, b));
-                }
+                *lines.entry((a, b)).or_insert(0) += 1;
             }
         }
     }
 
-    println!("{}", result_normal.len() + result_x.len());
+    let mut result = 0;
+
+    for (_, &count) in lines.iter() {
+        if count >= k * (k - 1) / 2 {
+            result += 1;
+        }
+    }
+
+    for (_, &count) in lines_x.iter() {
+        if count >= k * (k - 1) / 2 {
+            result += 1;
+        }
+    }
+
+    println!("{}", result);
 }
