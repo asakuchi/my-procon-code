478a00a036720996444ae11a82b4c9b46b285cce
asakuchi <paleApricot.asakuchi@gmail.com>
2022-04-24 23:08:23 +0900

    ABC249 D

--- atcoder/abc249/D/src/main.rs
+++ atcoder/abc249/D/src/main.rs
@@ -1,15 +1,5 @@
 use proconio::fastout;
 use proconio::input;
-use superslice::*;
-
-// use proconio::derive_readable;
-// use proconio::marker::Chars;
-// use proconio::marker::Usize1;
-// use itertools::izip;
-// use itertools::Itertools;
-// use petgraph::unionfind::UnionFind;
-
-use num_integer::*;
 
 #[fastout]
 fn main() {
@@ -19,59 +9,24 @@ fn main() {
     }
 
     a.sort();
-    // a.sort_by_key(|&x| std::cmp::Reverse(x));
 
-    eprintln!("{:?}", a);
+    let mut map = std::collections::HashMap::new();
 
-    let mut result = 0;
+    for &num in a.iter() {
+        *map.entry(num).or_insert(0) += 1;
+    }
 
-    for i in 0..n {
-        // let mut j = 0;
+    let mut result: usize = 0;
 
-        // while j < n && a[j] <= a[i] {
-        //     if a[i] % a[j] != 0 {
-        //         j += 1;
-        //         continue;
-        //     }
+    let m = 200_001;
 
-        //     let target = a[i] / a[j];
+    for a in 1..m {
+        for b in 1..=m / a {
+            let c = a * b;
 
-        //     let k_first = a.lower_bound(&target);
-
-        //     for k in k_first..n {
-        //         if k < n && a[i] == a[j] * a[k] {
-        //             // eprintln!("{} {} {}", i, j, k);
-        //             result += 1;
-        //         } else {
-        //             break;
-        //         }
-        //     }
-
-        //     j += 1;
-        // }
-
-        // let half = ((a[i] + 1) / 2) + 3;
-        let half = ((a[i] + 1) as f64).sqrt();
-
-        let j_max = a.lower_bound(&((half as usize) + 1));
-
-        for j in 0..=j_max {
-            if a[i] % a[j] != 0 {
-                continue;
-            }
-
-            let target = a[i] / a[j];
-
-            let k_first = a.lower_bound(&target);
-
-            for k in k_first..n {
-                if k < n && a[i] == a[j] * a[k] {
-                    eprintln!("{} {} {}", i, j, k);
-                    result += 1;
-                } else {
-                    break;
-                }
-            }
+            result += *map.entry(a).or_insert(0)
+                * *map.entry(b).or_insert(0)
+                * *map.entry(c).or_insert(0);
         }
     }
 
