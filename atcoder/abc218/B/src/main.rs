85cc238cc8ba895547e64d479ddaf9f60c548d22
asakuchi <paleApricot.asakuchi@gmail.com>
2022-04-26 19:35:58 +0900

    ABC218 B

---
+++ atcoder/abc218/B/src/main.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: [u8; 26],
+    }
+
+    for num in a {
+        print!("{}", ('a' as u8 + num - 1) as char);
+    }
+
+    println!();
+}
