28e9a72b25d36df5c5590b4565ab460801616e6c
asakuchi <paleApricot.asakuchi@gmail.com>
2022-05-24 09:25:00 +0900

    解きなおし

--- atcoder/abc128/C/src/main.rs
+++ atcoder/abc128/C/src/main.rs
@@ -1,46 +1,32 @@
-use proconio::fastout;
 use proconio::input;
+use proconio::marker::Usize1;
 
-#[fastout]
 fn main() {
     input! {
         n: usize,
         m: usize,
-    }
-
-    let mut s = vec![Vec::with_capacity(n); m];
-
-    for i in 0..m {
-        input! {
-            k:usize,
-            s_inner: [usize;k],
-        }
-
-        s[i] = s_inner.iter().map(|x| x - 1).collect();
-    }
-
-    input! {
+        k: [[Usize1]; m],
         p: [usize; m],
     }
 
     let mut result = 0;
 
-    for bit in 0..(1 << n) {
-        let mut all_on = true;
-        for j in 0..m {
-            let on_count = s[j]
-                .iter()
-                .filter(|&&x| bit & (1 << x) > 0)
-                .collect::<Vec<_>>()
-                .len();
+    'mask_loop: for mask in 0..1 << n {
+        for light in 0..m {
+            let mut on_count = 0;
 
-            if on_count % 2 != p[j] {
-                all_on = false;
+            for &switch in k[light].iter() {
+                if mask & 1 << switch > 0 {
+                    on_count += 1;
+                }
+            }
+
+            if on_count % 2 != p[light] {
+                continue 'mask_loop;
             }
         }
-        if all_on {
-            result += 1;
-        }
+
+        result += 1;
     }
 
     println!("{}", result);
