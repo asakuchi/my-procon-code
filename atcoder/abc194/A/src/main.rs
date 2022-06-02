74bea3ca0f47e6bc9d158465a8483db7572a5644
asakuchi <paleApricot.asakuchi@gmail.com>
2022-06-03 08:35:33 +0900

    ABC194 A

---
+++ atcoder/abc194/A/src/main.rs
@@ -0,0 +1,20 @@
+use proconio::fastout;
+use proconio::input;
+
+#[fastout]
+fn main() {
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+    if a + b >= 15 && b >= 8 {
+        println!("1");
+    } else if a + b >= 10 && b >= 3 {
+        println!("2");
+    } else if a + b >= 3 {
+        println!("3");
+    } else {
+        println!("4");
+    }
+}
