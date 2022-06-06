7f9dc3c60212d0d51ca80b8e2c1978b540ebf552
asakuchi <49332371+asakuchi@users.noreply.github.com>
2022-06-07 01:23:21 +0900

    ABC212 A

---
+++ atcoder/abc212/A/src/main.rs
@@ -0,0 +1,18 @@
+use proconio::fastout;
+use proconio::input;
+
+#[fastout]
+fn main() {
+    input! {
+        a: usize,
+        b: usize,
+    }
+
+  if a == 0 {
+    println!("Silver");
+  } else if b == 0 {
+    println!("Gold");
+  } else {
+    println!("Alloy");
+  }
+}
