6ad3e643e59445b8dcefd26079141c1ac7996d56
asakuchi <paleApricot.asakuchi@gmail.com>
2022-06-20 01:03:14 +0900

    次の盤面を用意

--- atcoder/abc256/B/src/main.rs
+++ atcoder/abc256/B/src/main.rs
@@ -11,20 +11,22 @@ fn main() {
     let mut p = 0;
 
     for &value in &a {
+        // マス0に駒を置く
         list[0] += 1;
 
-        for i in (0..4).rev() {
-            if value + i >= 4 {
-                if list[i] > 0 {
-                    p += 1;
-                }
+        let mut next_list = vec![0; 4];
 
-                list[i] = 0;
-            } else {
-                list[i + value] = list[i];
-                list[i] = 0;
+        for i in 0..4 {
+            if list[i] > 0 {
+                if value + i >= 4 {
+                    p += 1;
+                } else {
+                    next_list[i + value] = list[i];
+                }
             }
         }
+
+        list = next_list;
     }
 
     println!("{}", p);
