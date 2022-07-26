0d05ddabd81fe0a1383576076adfd2e65218361a
asakuchi <paleapricot.asakuchi@gmail.com>
2022-07-26 19:51:18 +0900

    abc200_b

---
+++ abc/src/bin/abc200_b.rs
@@ -0,0 +1,18 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        mut n: usize,
+        k: usize,
+    }
+
+    for _ in 0..k {
+        if n % 200 == 0 {
+            n /= 200;
+        } else {
+            n = format!("{}200", n.to_string(),).parse().unwrap();
+        }
+    }
+
+    println!("{}", n);
+}
