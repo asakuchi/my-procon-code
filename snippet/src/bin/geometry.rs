06ba456ce6c22ba86914c4729b39580646a5a7e1
asakuchi <paleapricot.asakuchi@gmail.com>
2022-08-30 01:03:34 +0900

    座標等追加

--- snippet/src/bin/geometry.rs
+++ snippet/src/bin/geometry.rs
@@ -3,12 +3,102 @@
 //!
 
 fn main() {
+    let p1 = Point2(1., 1.);
+    let p2 = Point2(2., 3.);
+
+    println!("{:?}", p1 + p2);
+    println!("{:?}", p1 - p2);
+    println!("{:?}", p1 * 3.);
+
     println!("{}", cos_formula(3., 4., 0.5 * std::f64::consts::PI,));
-
-    println!("{}", distance(0., 4., -3., 0.));
-
     println!("{:?}", rotate(1., 2., 3., 4., 0.5 * std::f64::consts::PI));
 }
+
+const EPS: f64 = 1e-10;
+
+/// 座標
+#[derive(Copy, Clone, Debug)]
+struct Point2(f64, f64);
+
+impl std::ops::Add for Point2 {
+    type Output = Point2;
+
+    fn add(self, rhs: Self) -> Self {
+        Point2(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl std::ops::Sub for Point2 {
+    type Output = Point2;
+
+    fn sub(self, rhs: Self) -> Self {
+        Point2(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl std::ops::Mul<f64> for Point2 {
+    type Output = Point2;
+
+    fn mul(self, k: f64) -> Self {
+        Point2(self.0 * k, self.1 * k)
+    }
+}
+
+impl Point2 {
+    /// ノルム
+    /// ベクトルの大きさ
+    fn norm(&self) -> f64 {
+        self.0 * self.0 + self.1 * self.1
+    }
+
+    /// ノルム
+    /// ベクトルの大きさ
+    fn abs(&self) -> f64 {
+        self.norm().abs()
+    }
+
+    /// マンハッタン距離
+    /// 2点間の距離
+    fn manhattan_distance(&self, rhs: Point2) -> f64 {
+        (self.0 - rhs.0).abs() + (self.1 - rhs.1).abs()
+    }
+
+    /// ユークリッド距離
+    /// 2点間の距離
+    fn euclidean_distance(&self, rhs: Point2) -> f64 {
+        ((self.0 - rhs.0).powf(2.) + (self.1 - rhs.1).powf(2.)).sqrt()
+    }
+
+    /// 内積
+    fn dot(&self, rhs: Point2) -> f64 {
+        self.0 * rhs.0 + self.1 * rhs.1
+    }
+
+    /// 外積
+    fn cross(&self, rhs: Point2) -> f64 {
+        self.0 * rhs.1 - self.1 * rhs.0
+    }
+
+    /// 2つのベクトルが直交するか
+    fn is_orthogonal(&self, rhs: Point2) -> bool {
+        self.dot(rhs).abs() < EPS
+    }
+}
+
+/// 線分
+struct Segment2(Point2, Point2);
+
+/// 直線
+struct Line2(Segment2);
+
+/// 円
+struct Circle {
+    center: Point2,
+    radius: f64,
+}
+
+/// 多角形
+struct Polygon(Vec<Point2>);
 
 ///
 /// 余弦定理
@@ -19,13 +109,6 @@ fn main() {
 ///
 fn cos_formula(a: f64, b: f64, c_angle_radian: f64) -> f64 {
     (a * a + b * b - 2. * a * b * c_angle_radian.cos()).sqrt()
-}
-
-///
-/// 2点間の距離
-///
-fn distance(x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
-    ((x1 - x2).powf(2.) + (y1 - y2).powf(2.)).sqrt()
 }
 
 ///
