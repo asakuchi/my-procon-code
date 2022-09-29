a473b1897e753802754000629e45750d5516d665
asakuchi <paleapricot.asakuchi@gmail.com>
2022-09-29 22:31:26 +0900

    abc171_b

---
+++ abc/src/bin/abc171_b.rs
@@ -0,0 +1,15 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        mut p: [usize; n],
+    }
+
+    p.sort();
+
+    let result: usize = p[0..k].iter().sum();
+
+    println!("{}", result);
+}
