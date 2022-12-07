f2eef036bd30fa94c8899ef195df25fb156d7c16
asakuchi <paleapricot.asakuchi@gmail.com>
2022-12-07 19:03:29 +0900

    三分探索

---
+++ snippet/src/bin/ternary-search.rs
@@ -0,0 +1,25 @@
+//!
+//! 三分探索
+//!
+
+fn main() {
+    let mut low = 0;
+    let mut high = 1_000_000_000_000_000_000;
+
+    while high - low > 3 {
+        let c1 = (low * 2 + high) / 3;
+        let c2 = (low + high * 2) / 3;
+
+        if calc(c1) > calc(c2) {
+            low = c1;
+        } else {
+            high = c2
+        }
+    }
+
+    println!("{}", low);
+}
+
+fn calc(time: usize) -> f64 {
+    todo!();
+}
