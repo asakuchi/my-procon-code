77ee707b79782a922ba72170eeaa0ad89f88edee
asakuchi <paleapricot.asakuchi@gmail.com>
2022-12-07 19:05:02 +0900

    コメント追加

--- snippet/src/bin/binary-search.rs
+++ snippet/src/bin/binary-search.rs
@@ -1,6 +1,13 @@
+//!
+//! 二分探索
+//!
+
 fn main() {
     let mut ok: isize = 1 << 60;
     let mut ng = -1;
+
+    // 実数で探索する場合の終了条件
+    // for _ in 0..100 {
 
     while (ok - ng).abs() > 1 {
         let mid = (ok + ng) / 2;
--- snippet/src/bin/ternary-search.rs
+++ snippet/src/bin/ternary-search.rs
@@ -6,6 +6,9 @@ fn main() {
     let mut low = 0;
     let mut high = 1_000_000_000_000_000_000;
 
+    // 実数で探索する場合の終了条件
+    // for _ in 0..100 {
+
     while high - low > 3 {
         let c1 = (low * 2 + high) / 3;
         let c2 = (low + high * 2) / 3;
