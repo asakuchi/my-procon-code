87497733e31861fe44baf4b6d6b63ad13461e53d
asakuchi <paleapricot.asakuchi@gmail.com>
2022-12-09 22:28:01 +0900

    テンプレート変更

--- aoj/src/bin/template_a.rs
+++ aoj/src/bin/template_a.rs
@@ -1,24 +1,32 @@
 fn main() {
-    input_usize();
-    input_tuple();
-    input_vec_2d(10);
-    input_tuple_vec(10);
+    let _x: usize = input_value();
+    let (_n, _m): (usize, usize) = input_tuple();
+    // input_vec_2d(10);
+    // input_tuple_vec(10);
     input_char_vec();
 }
 
-fn input_usize() -> usize {
+fn input_value<T>() -> T
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
     let stdin = std::io::stdin();
 
     let mut buf = String::new();
     stdin.read_line(&mut buf).unwrap();
     buf = buf.trim_end().to_owned();
 
-    let n: usize = buf.parse().unwrap();
+    let n = buf.parse().unwrap();
 
     n
 }
 
-fn input_tuple() -> (usize, usize) {
+fn input_tuple<T>() -> (T, T)
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
     let stdin = std::io::stdin();
 
     let mut buf = String::new();
@@ -27,13 +35,17 @@ fn input_tuple() -> (usize, usize) {
 
     let mut iter = buf.split_whitespace();
 
-    let n: usize = iter.next().unwrap().parse().unwrap();
-    let m: usize = iter.next().unwrap().parse().unwrap();
+    let n = iter.next().unwrap().parse().unwrap();
+    let m = iter.next().unwrap().parse().unwrap();
 
     (n, m)
 }
 
-fn input_vec() -> Vec<usize> {
+fn input_vec<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
     let stdin = std::io::stdin();
 
     let mut buf = String::new();
@@ -42,18 +54,21 @@ fn input_vec() -> Vec<usize> {
 
     let iter = buf.split_whitespace();
 
-    let line: Vec<_> = iter.map(|x| x.parse().unwrap()).collect();
+    let line = iter.map(|x| x.parse().unwrap()).collect();
 
     line
 }
 
-
-fn input_vec_2d(n: usize) -> Vec<Vec<isize>> {
+fn input_vec_2d<T>(n: usize) -> Vec<Vec<T>>
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
     // 二次元ベクタ
 
     let stdin = std::io::stdin();
 
-    let mut a: Vec<Vec<isize>> = Vec::with_capacity(n);
+    let mut a = Vec::with_capacity(n);
 
     for _ in 0..n {
         let mut buf = String::new();
@@ -62,7 +77,7 @@ fn input_vec_2d(n: usize) -> Vec<Vec<isize>> {
 
         let iter = buf.split_whitespace();
 
-        let line: Vec<isize> = iter.map(|x| x.parse().unwrap()).collect();
+        let line = iter.map(|x| x.parse().unwrap()).collect();
 
         a.push(line);
     }
@@ -70,12 +85,16 @@ fn input_vec_2d(n: usize) -> Vec<Vec<isize>> {
     a
 }
 
-fn input_tuple_vec(n: usize) -> Vec<(usize, usize, usize)> {
+fn input_tuple_vec<T>(n: usize) -> Vec<(T, T, T)>
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
     // タプルのベクタ
 
     let stdin = std::io::stdin();
 
-    let mut s_t_d: Vec<(usize, usize, usize)> = Vec::with_capacity(n);
+    let mut s_t_d = Vec::with_capacity(n);
 
     for _ in 0..n {
         let mut buf = String::new();
@@ -84,9 +103,9 @@ fn input_tuple_vec(n: usize) -> Vec<(usize, usize, usize)> {
 
         let mut iter = buf.split_whitespace();
 
-        let s: usize = iter.next().unwrap().parse().unwrap();
-        let t: usize = iter.next().unwrap().parse().unwrap();
-        let d: usize = iter.next().unwrap().parse().unwrap();
+        let s = iter.next().unwrap().parse().unwrap();
+        let t = iter.next().unwrap().parse().unwrap();
+        let d = iter.next().unwrap().parse().unwrap();
 
         s_t_d.push((s, t, d));
     }
@@ -101,7 +120,7 @@ fn input_char_vec() -> Vec<char> {
     stdin.read_line(&mut buf).unwrap();
     buf = buf.trim_end().to_owned();
 
-    let x: Vec<char> = buf.chars().collect();
+    let x = buf.chars().collect();
 
     x
 }
