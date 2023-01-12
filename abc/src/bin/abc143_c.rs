d73ccd38da2eb341640cc21ac33fe8e579f9d766
asakuchi <paleapricot.asakuchi@gmail.com>
2023-01-12 19:03:17 +0900

    ランレングス

--- abc/src/bin/abc143_c.rs
+++ abc/src/bin/abc143_c.rs
@@ -1,23 +1,13 @@
+use ::procon_library_rs::run_length_encoding::run_length_encoding;
 use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
         _n: usize,
-        mut s: Chars,
+        s: Chars,
     }
 
-    let mut result = 0;
+    let list = run_length_encoding(&s);
 
-    while let Some(slime) = s.pop() {
-        while let Some(next_slime) = s.pop() {
-            if slime != next_slime {
-                s.push(next_slime);
-                break;
-            }
-        }
-
-        result += 1;
-    }
-
-    println!("{}", result);
+    println!("{}", list.len());
 }
