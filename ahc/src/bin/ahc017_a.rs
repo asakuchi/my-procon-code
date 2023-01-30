1165e8296d6cc05ed77f03b8296849356805cad5
asakuchi <paleapricot.asakuchi@gmail.com>
2023-01-31 00:34:37 +0900

    最小全域木

--- ahc/src/bin/ahc017_a.rs
+++ ahc/src/bin/ahc017_a.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::Usize1};
 
 fn main() {
@@ -7,20 +8,60 @@ fn main() {
         m: usize,
         d: usize,
         k: usize,
-        u_v_w: [(Usize1, Usize1, usize); m],
-        x_y: [(usize, usize); n],
+        mut u_v_w: [(Usize1, Usize1, usize); m],
+        _x_y: [(usize, usize); n],
     }
 
-    // とりあえずスコアを獲得する提出
-    let mut result = Vec::new();
+    // 最小全域木
+    let mut min_spanning_tree = Vec::new();
+    // その他
+    let mut others = Vec::new();
 
-    for day in 1..=d {
-        for _ in 0..k {
-            result.push(day);
+    // 重みの昇順にソート
+    u_v_w.sort_by_key(|x| x.2);
+
+    let mut set = UnionFind::new(n);
+
+    for i in 0..m {
+        let (u, v, _w) = u_v_w[i];
+
+        if !set.equiv(u, v) {
+            min_spanning_tree.push(i);
+            set.union(u, v);
+        } else {
+            others.push(i);
         }
     }
 
-    // println!("{:?}", &result[..m]);
+    // eprintln!("mst {:?}", min_spanning_tree);
+    eprintln!("mst {}", min_spanning_tree.len());
+
+    // 各日に工事する道路
+    let mut plan_day = vec![Vec::new(); d];
+
+    let mut days = (0..d).cycle();
+
+    // 最小全域木を均等に割り振る
+    for &load in min_spanning_tree.iter() {
+        let day = days.next().unwrap();
+
+        plan_day[day].push(load);
+    }
+
+    // その他も均等に割り振る
+    for &load in others.iter() {
+        let day = days.next().unwrap();
+
+        plan_day[day].push(load);
+    }
+
+    let mut result = vec![0; m];
+
+    for day in 0..d {
+        for &load in &plan_day[day] {
+            result[load] = day + 1;
+        }
+    }
 
     let text = &result[..m].iter().format(" ");
 
