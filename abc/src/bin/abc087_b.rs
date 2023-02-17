f93849300d6d3805a82e4823f6357ceb745d30c1
asakuchi <paleapricot.asakuchi@gmail.com>
2023-02-17 23:41:13 +0900

    abc087_b

---
+++ abc/src/bin/abc087_b.rs
@@ -0,0 +1,24 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        x: usize,
+    }
+
+    let mut result = 0;
+
+    for i in 0..=a {
+        for j in 0..=b {
+            for k in 0..=c {
+                if 500 * i + 100 * j + 50 * k == x {
+                    result += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", result);
+}
