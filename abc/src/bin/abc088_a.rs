754fb4cf296f262ba0781fe7694986b29188b9a9
asakuchi <paleapricot.asakuchi@gmail.com>
2023-03-01 23:57:29 +0900

    abc088_a

---
+++ abc/src/bin/abc088_a.rs
@@ -0,0 +1,14 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        a: usize,
+    }
+
+    if n % 500 <= a {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
