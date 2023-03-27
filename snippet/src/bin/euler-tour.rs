e6718e765f72d94117ef4ae22fbe1bab5a87ce49
asakuchi <paleapricot.asakuchi@gmail.com>
2023-03-27 21:51:42 +0900

    refactor

--- snippet/src/bin/euler-tour.rs
+++ snippet/src/bin/euler-tour.rs
@@ -1,10 +1,10 @@
-use ac_library_rs::{Additive, Segtree};
+//!
+//! オイラーツアー
+//!
+//! https://maspypy.com/euler-tour-%E3%81%AE%E3%81%8A%E5%8B%89%E5%BC%B7
+//!
 
-#[derive(Debug)]
-enum EdgeDirection {
-    Up(usize),
-    Down(usize),
-}
+use ac_library_rs::{Additive, Segtree};
 
 fn main() {
     let n = 6;
@@ -23,84 +23,114 @@ fn main() {
 
     println!("{:?}", list);
 
-    let mut visited = vec![false; n];
-    visited[0] = true;
-
-    let mut edge = Vec::new();
-    let mut weight = Vec::new();
-
-    edge.push(EdgeDirection::Down(0));
-    weight.push(0);
-
-    dfs(n, m, &list, 0, &mut visited, &mut edge, &mut weight);
-
-    edge.push(EdgeDirection::Up(0));
-    weight.push(0);
-
-    println!("edge {:?}", edge);
-
-    let mut in_list = vec![0; n];
-    let mut out_list = vec![0; n];
-
-    let mut visited = vec![false; n];
-
-    for i in 0..edge.len() {
-        match edge[i] {
-            EdgeDirection::Down(edge_num) => {
-                if visited[edge_num] {
-                    continue;
-                }
-
-                visited[edge_num] = true;
-
-                in_list[edge_num] = i;
-            }
-            EdgeDirection::Up(edge_num) => {
-                out_list[edge_num] = i;
-            }
-        }
-    }
-
-    println!("in :{:?}", in_list);
-    println!("out: {:?}", out_list);
-
-    let mut path_query_tree = Segtree::<Additive<isize>>::new(edge.len());
-
-    for i in 0..edge.len() {
-        path_query_tree.set(i, weight[i]);
-    }
+    let tour = EulerTour::new(n, m, &list);
 
     for i in 0..n {
         println!(
             "根から頂点{}までのパスの重み {}",
             i + 1,
-            path_query_tree.prod(0, in_list[i] + 1)
+            // path_query_tree.prod(0, in_list[i] + 1)
+            tour.path_query(i),
         );
     }
 }
 
-fn dfs(
-    n: usize,
-    m: usize,
-    list: &Vec<Vec<(usize, usize)>>,
-    current: usize,
-    visited: &mut Vec<bool>,
-    edge: &mut Vec<EdgeDirection>,
-    weight: &mut Vec<isize>,
-) {
-    for &(next, cost) in list[current].iter() {
-        if visited[next] {
-            continue;
+#[derive(Debug)]
+enum EdgeDirection {
+    Up(usize),
+    Down(usize),
+}
+
+struct EulerTour {
+    path_query_tree: Segtree<Additive<isize>>,
+    in_list: Vec<usize>,
+}
+
+impl EulerTour {
+    fn new(n: usize, m: usize, list: &Vec<Vec<(usize, isize)>>) -> EulerTour {
+        let mut visited = vec![false; n];
+        visited[0] = true;
+
+        let mut edge = Vec::new();
+        let mut weight = Vec::new();
+
+        edge.push(EdgeDirection::Down(0));
+        weight.push(0);
+
+        EulerTour::dfs(n, m, list, 0, &mut visited, &mut edge, &mut weight);
+
+        edge.push(EdgeDirection::Up(0));
+        weight.push(0);
+
+        // println!("edge {:?}", edge);
+
+        let mut in_list = vec![0; n];
+        let mut out_list = vec![0; n];
+
+        let mut visited = vec![false; n];
+
+        for i in 0..edge.len() {
+            match edge[i] {
+                EdgeDirection::Down(edge_num) => {
+                    if visited[edge_num] {
+                        continue;
+                    }
+
+                    visited[edge_num] = true;
+
+                    in_list[edge_num] = i;
+                }
+                EdgeDirection::Up(edge_num) => {
+                    out_list[edge_num] = i;
+                }
+            }
         }
 
-        visited[next] = true;
+        // println!("in :{:?}", in_list);
+        // println!("out: {:?}", out_list);
 
-        edge.push(EdgeDirection::Down(next));
-        weight.push(cost as isize);
+        let mut path_query_tree = Segtree::<Additive<isize>>::new(edge.len());
 
-        dfs(n, m, list, next, visited, edge, weight);
+        for i in 0..edge.len() {
+            path_query_tree.set(i, weight[i]);
+        }
 
-        edge.push(EdgeDirection::Up(next));
-        weight.push(-(cost as isize));
+        EulerTour {
+            path_query_tree,
+            in_list,
+        }
+    }
+
+    fn dfs(
+        n: usize,
+        m: usize,
+        list: &Vec<Vec<(usize, isize)>>,
+        current: usize,
+        visited: &mut Vec<bool>,
+        edge: &mut Vec<EdgeDirection>,
+        weight: &mut Vec<isize>,
+    ) {
+        for &(next, cost) in list[current].iter() {
+            if visited[next] {
+                continue;
+            }
+
+            visited[next] = true;
+
+            edge.push(EdgeDirection::Down(next));
+            weight.push(cost);
+
+            EulerTour::dfs(n, m, list, next, visited, edge, weight);
+
+            edge.push(EdgeDirection::Up(next));
+            weight.push(-cost);
+        }
+    }
+
+    ///
+    /// 根から頂点vまでのパスの重み
+    ///
+    fn path_query(&self, v: usize) -> isize {
+        self.path_query_tree.prod(0, self.in_list[v] + 1)
     }
 }
