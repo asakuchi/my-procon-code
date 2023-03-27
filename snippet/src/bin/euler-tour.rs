e651bc67e7875ed96075f965727b0326c3004992
asakuchi <paleapricot.asakuchi@gmail.com>
2023-03-27 21:43:20 +0900

    refactoring

--- snippet/src/bin/euler-tour.rs
+++ snippet/src/bin/euler-tour.rs
@@ -1,20 +1,9 @@
-use ac_library_rs::{Additive, Monoid, Segtree};
+use ac_library_rs::{Additive, Segtree};
 
-// struct DepthIndex(usize, usize);
-
-pub struct DepthIndexMin;
-
-impl Monoid for DepthIndexMin {
-    // type S = S;
-    type S = (usize, usize);
-
-    fn identity() -> Self::S {
-        // S::max_value()
-        (usize::MAX, 0)
-    }
-    fn binary_operation(a: &Self::S, b: &Self::S) -> Self::S {
-        std::cmp::min(*a, *b)
-    }
+#[derive(Debug)]
+enum EdgeDirection {
+    Up(usize),
+    Down(usize),
 }
 
 fn main() {
@@ -23,36 +12,52 @@ fn main() {
 
     let u_v_w = vec![(1, 2, 1), (2, 3, 2), (3, 4, 4), (2, 5, 8), (1, 6, 16)];
 
-    let mut list = vec![Vec::new(); n + 1];
+    let mut list = vec![Vec::new(); n];
 
-    for &(u, v, w) in &u_v_w {
-        list[u].push((v, w));
-        list[v].push((u, w));
+    for i in 0..m {
+        let (u, v, w) = u_v_w[i];
+
+        list[u - 1].push((v - 1, w));
+        list[v - 1].push((u - 1, w));
     }
 
     println!("{:?}", list);
 
-    let mut visited = vec![false; n + 1];
-    visited[1] = true;
+    let mut visited = vec![false; n];
+    visited[0] = true;
 
     let mut edge = Vec::new();
+    let mut weight = Vec::new();
 
-    edge.push((1, 0, 0));
+    edge.push(EdgeDirection::Down(0));
+    weight.push(0);
 
-    dfs(n, m, &list, 1, &mut visited, &mut edge, 0, 0);
+    dfs(n, m, &list, 0, &mut visited, &mut edge, &mut weight);
 
-    edge.push((-1, 0, 0));
+    edge.push(EdgeDirection::Up(0));
+    weight.push(0);
 
     println!("edge {:?}", edge);
 
-    let mut in_list = vec![0; n + 1];
-    let mut out_list = vec![0; n + 1];
+    let mut in_list = vec![0; n];
+    let mut out_list = vec![0; n];
+
+    let mut visited = vec![false; n];
 
     for i in 0..edge.len() {
-        if edge[i].0 > 0 {
-            in_list[edge[i].0 as usize] = i;
-        } else {
-            out_list[(-edge[i].0) as usize] = i;
+        match edge[i] {
+            EdgeDirection::Down(edge_num) => {
+                if visited[edge_num] {
+                    continue;
+                }
+
+                visited[edge_num] = true;
+
+                in_list[edge_num] = i;
+            }
+            EdgeDirection::Up(edge_num) => {
+                out_list[edge_num] = i;
+            }
         }
     }
 
@@ -61,33 +66,17 @@ fn main() {
 
     let mut path_query_tree = Segtree::<Additive<isize>>::new(edge.len());
 
-    let mut lca_tree = Segtree::<DepthIndexMin>::new(edge.len());
-
     for i in 0..edge.len() {
-        path_query_tree.set(i, edge[i].1);
-        lca_tree.set(i, (edge[i].2, i));
+        path_query_tree.set(i, weight[i]);
     }
 
-    println!(
-        "根から頂点5までのパスの重み {}",
-        path_query_tree.prod(0, in_list[5] + 1)
-    );
-
-    println!(
-        "根から頂点3までのパスの重み {}",
-        path_query_tree.prod(0, in_list[3] + 1)
-    );
-
-    let index = lca_tree.prod(in_list[3], out_list[5] + 1).1;
-
-    println!("頂点3と頂点5のLCA {:?}", edge[index].0.abs());
-    // これの始点がLCA
-    // TODO: 辺の始点を求める必要あり
-
-    println!(
-        "頂点3から頂点5までのパスの重み {}",
-        path_query_tree.prod(out_list[3], in_list[5] + 1)
-    );
+    for i in 0..n {
+        println!(
+            "根から頂点{}までのパスの重み {}",
+            i + 1,
+            path_query_tree.prod(0, in_list[i] + 1)
+        );
+    }
 }
 
 fn dfs(
@@ -96,12 +85,9 @@ fn dfs(
     list: &Vec<Vec<(usize, usize)>>,
     current: usize,
     visited: &mut Vec<bool>,
-    edge: &mut Vec<(isize, isize, usize)>,
-    total_cost: usize,
-    depth: usize,
+    edge: &mut Vec<EdgeDirection>,
+    weight: &mut Vec<isize>,
 ) {
-    // edge.push((current as isize, total_cost));
-
     for &(next, cost) in list[current].iter() {
         if visited[next] {
             continue;
@@ -109,21 +95,12 @@ fn dfs(
 
         visited[next] = true;
 
-        edge.push((next as isize, cost as isize, depth + 1));
+        edge.push(EdgeDirection::Down(next));
+        weight.push(cost as isize);
 
-        dfs(
-            n,
-            m,
-            list,
-            next,
-            visited,
-            edge,
-            total_cost + cost,
-            depth + 1,
-        );
+        dfs(n, m, list, next, visited, edge, weight);
 
-        edge.push((-(next as isize), -(cost as isize), depth));
+        edge.push(EdgeDirection::Up(next));
+        weight.push(-(cost as isize));
     }
-
-    // edge.push((-(current as isize), total_cost));
 }
