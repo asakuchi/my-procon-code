fcc1d8cf8dd1abbc60e37078a4244b53ca700f9f
asakuchi <paleapricot.asakuchi@gmail.com>
2023-04-23 21:39:49 +0900

    update template

--- work/src/bin/f.rs
+++ work/src/bin/f.rs
@@ -1,24 +1,14 @@
 use proconio::input;
 // use proconio::fastout;
-// use proconio::derive_readable;
-// use proconio::marker::Chars;
-// use proconio::marker::Usize1;
-// use itertools::izip;
-// use itertools::Itertools;
-// use petgraph::unionfind::UnionFind;
 
 // #[fastout]
 fn main() {
     input! {
         n: usize,
-        m: usize,
         a: [usize; n],
-        p: [[usize; m]; n],
-        mut plan: [(usize, usize); n],
-        s: String,
-
+        // m: usize,
+        // u_v: [(Usize1, Usize1); n],
+        // p: [[usize; w]; h],
+        // s: String,
     }
-
-    // println!("{}", if yes { "Yes" } else { "No" });
-    // println!("{}", yes);
 }
