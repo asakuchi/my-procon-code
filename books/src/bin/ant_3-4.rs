023da6a688e9f5d7ac3c160f295ff74767acd0ea
asakuchi <paleapricot.asakuchi@gmail.com>
2023-05-15 19:01:33 +0900

    配るDP追加

--- books/src/bin/ant_3-4.rs
+++ books/src/bin/ant_3-4.rs
@@ -46,9 +46,14 @@ fn main() {
     println!("{}", result);
 
     // ------- 漸化式 -------
+    // 全て訪問済みのスタートを0にし、
+    // そこから逆算してコストを増やしていく
+    // 全て未訪問のスタートが答え
 
+    // 全て訪問済みのスタートへ行くまでに必要なコスト
     let mut dp = vec![vec![INF; MAX_N]; 1 << MAX_N];
 
+    // 全て訪問済みのスタート
     dp[(1 << n) - 1][0] = 0;
 
     for s in (0..=(1 << n) - 2).rev() {
@@ -63,7 +68,58 @@ fn main() {
         }
     }
 
+    // 全て未訪問のスタート
     println!("{}", dp[0][0]);
+
+    // ------- 漸化式 -------
+    // ゴール頂点を追加する
+    // スタートのみ訪問済みのスタートから重みを加算していく
+    // 全て訪問済みのゴールが答え
+
+    // スタートのみ訪問済みのスタートからの重みの合計
+    let mut dp = vec![vec![INF; MAX_N]; 1 << MAX_N];
+
+    let mut costs = vec![vec![0; n + 1]; n + 1];
+
+    // 入力値を修正
+    for i in 0..n {
+        for j in 0..n {
+            if j == 0 {
+                // スタートへ向かう有向辺をINFにし、
+                // 代わりにゴールへの有向辺を追加する
+                costs[i][0] = INF;
+
+                if d[i][j] == -1 {
+                    costs[i][n] = INF;
+                } else {
+                    costs[i][n] = d[i][j] as usize;
+                }
+            } else {
+                if d[i][j] == -1 {
+                    costs[i][j] = INF;
+                } else {
+                    costs[i][j] = d[i][j] as usize;
+                }
+            }
+        }
+    }
+
+    // ゴール地点を追加
+    let n = n + 1;
+
+    // スタートのみ訪問済みのスタート
+    dp[1 << 0][0] = 0;
+
+    for s in 0..1 << n {
+        for v in 0..n {
+            for u in 0..n {
+                dp[s | 1 << u][u] = dp[s | 1 << u][u].min(dp[s][v] + costs[v][u]);
+            }
+        }
+    }
+
+    // 全て訪問済みのゴール
+    println!("{}", dp[(1 << n) - 1][n - 1]);
 }
 
 fn rec(
