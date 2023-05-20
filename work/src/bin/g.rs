7ebda3cc7b0c5e6529a275e724639309649632a4
asakuchi <paleapricot.asakuchi@gmail.com>
2023-05-20 23:50:00 +0900

    add g

---
+++ work/src/bin/g.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+// #[proconio::fastout]
+fn main() {
+    input! {
+        n: usize,
+        a: [usize; n],
+        // m: usize,
+        // u_v: [(Usize1, Usize1); n],
+        // p: [[usize; w]; h],
+        // s: String,
+    }
+}
