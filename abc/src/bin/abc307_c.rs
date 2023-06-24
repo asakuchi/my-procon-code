70814964b90b1efaf238c03904a17d609e2bcc7f
asakuchi <paleapricot.asakuchi@gmail.com>
2023-06-24 23:23:07 +0900

    abc307_c

--- abc/src/bin/abc307_c.rs
+++ abc/src/bin/abc307_c.rs
@@ -1,6 +1,6 @@
-use proconio::{input, marker::Chars};
+use std::collections::HashSet;
 
-const SIZE: usize = 20;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
@@ -15,55 +15,51 @@ fn main() {
         x: [Chars; h_x],
     }
 
-    'search: for i_x in 0..h_x {
-        for j_x in 0..w_x {
-            let mut filled = vec![vec!['.'; SIZE]; SIZE];
+    let mut orig_set_a = HashSet::new();
 
-            // (i_x, j_x) からシートAを貼ってみる
-            for i_a in 0..h_a {
-                for j_a in 0..w_a {
-                    if a[i_a][j_a] == '#' {
-                        filled[i_x + i_a][j_x + j_a] = '#';
-
-                        // 外はNG
-                        if (i_x + i_a >= h_x) || (j_x + j_a >= w_x) {
-                            continue 'search;
-                        }
-                    }
-                }
+    for i in 0..h_a {
+        for j in 0..w_a {
+            if a[i][j] == '#' {
+                orig_set_a.insert((i as isize, j as isize));
             }
+        }
+    }
 
-            // 更に 別の (i_x, j_x) から シートB を貼ってみる
-            'search_b: for i_x_2 in 0..h_x {
-                for j_x_2 in 0..w_x {
-                    let mut filled_2 = filled.clone();
+    let mut orig_set_b = HashSet::new();
 
-                    for i_b in 0..h_b {
-                        for j_b in 0..w_b {
-                            if b[i_b][j_b] == '#' {
-                                filled_2[i_x_2 + i_b][j_x_2 + j_b] = '#';
+    for i in 0..h_b {
+        for j in 0..w_b {
+            if b[i][j] == '#' {
+                orig_set_b.insert((i as isize, j as isize));
+            }
+        }
+    }
 
-                                // 外はNG
-                                if (i_x_2 + i_b >= h_x) || (j_x_2 + j_b >= w_x) {
-                                    continue 'search_b;
-                                }
-                            }
-                        }
+    let mut set_x = HashSet::new();
+
+    for i in 0..h_x {
+        for j in 0..w_x {
+            if x[i][j] == '#' {
+                set_x.insert((i as isize, j as isize));
+            }
+        }
+    }
+
+    for diff_a_i in -(h_x as isize)..h_x as isize {
+        for diff_a_j in -(w_x as isize)..w_x as isize {
+            for diff_b_i in -(h_x as isize)..h_x as isize {
+                for diff_b_j in -(w_x as isize)..w_x as isize {
+                    let mut set_c = HashSet::new();
+
+                    for &(i, j) in orig_set_a.iter() {
+                        set_c.insert((i + diff_a_i, j + diff_a_j));
                     }
 
-                    // シートXの # を全て含むか
-                    let mut ok = true;
-
-                    for i in 0..h_x {
-                        for j in 0..w_x {
-                            if x[i][j] != filled_2[i][j] {
-                                ok = false;
-                                break;
-                            }
-                        }
+                    for &(i, j) in orig_set_b.iter() {
+                        set_c.insert((i + diff_b_i, j + diff_b_j));
                     }
 
-                    if ok {
+                    if set_c == set_x {
                         println!("Yes");
                         return;
                     }
