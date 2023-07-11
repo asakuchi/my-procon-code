bb3f8d9de312a7ef7f581ef1784bd716f95f4bed
asakuchi <paleapricot.asakuchi@gmail.com>
2023-07-12 00:04:19 +0900

    abc191_a

---
+++ abc/src/bin/abc191_a.rs
@@ -0,0 +1,16 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        v: usize,
+        t: usize,
+        s: usize,
+        d: usize,
+    }
+
+    if v * t <= d && d <= v * s {
+        println!("No");
+    } else {
+        println!("Yes");
+    }
+}
