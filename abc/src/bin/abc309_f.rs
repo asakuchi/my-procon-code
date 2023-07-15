d7b8a1ecb786d138045d33ae5364645b961fa3d1
asakuchi <paleapricot.asakuchi@gmail.com>
2023-07-15 19:13:39 +0900

    座標圧縮

--- abc/src/bin/abc309_f.rs
+++ abc/src/bin/abc309_f.rs
@@ -1,8 +1,39 @@
-use std::collections::{BTreeMap, BTreeSet, HashMap};
+use std::{
+    collections::{BTreeMap, BTreeSet, HashMap},
+    hash::Hash,
+};
 
 use ac_library_rs::{Min, Segtree};
 
 use proconio::input;
+
+struct CoordinateCompression<T> {
+    coordinate: HashMap<T, usize>,
+}
+
+impl<T> CoordinateCompression<T>
+where
+    T: Eq + Hash + Ord + Copy,
+{
+    fn new(list: &Vec<T>) -> Self {
+        let mut numbers = BTreeSet::new();
+        let mut coordinate = HashMap::new();
+
+        for &x in list.iter() {
+            numbers.insert(x);
+        }
+
+        for (i, &x) in numbers.iter().enumerate() {
+            coordinate.insert(x, i + 1);
+        }
+
+        CoordinateCompression { coordinate }
+    }
+
+    fn get(&self, x: &T) -> Option<&usize> {
+        self.coordinate.get(x)
+    }
+}
 
 fn main() {
     input! {
@@ -10,10 +41,9 @@ fn main() {
         h_w_d: [(usize, usize, usize); n],
     }
 
-    // 座標圧縮
-    let mut numbers = BTreeSet::new();
-
     let mut map = BTreeMap::new();
+
+    let mut numbers = Vec::new();
 
     for &(h, w, d) in &h_w_d {
         let mut line = vec![h, w, d];
@@ -24,21 +54,17 @@ fn main() {
             .push((line[1], line[2]));
 
         // 座標圧縮
-        numbers.insert(line[1]);
+        numbers.push(line[1]);
     }
 
     // 座標圧縮
-    let mut coordinate = HashMap::new();
-
-    for (i, &x) in numbers.iter().enumerate() {
-        coordinate.insert(x, i + 1);
-    }
+    let cc = CoordinateCompression::new(&numbers);
 
     let mut tree = Segtree::<Min<usize>>::new(n * 3 + 1);
 
     for (_a, bc) in map.iter() {
         for &(b, c) in bc.iter() {
-            let cc_b = *coordinate.get(&b).unwrap();
+            let &cc_b = cc.get(&b).unwrap();
 
             if tree.prod(0, cc_b) < c {
                 println!("Yes");
@@ -47,7 +73,7 @@ fn main() {
         }
 
         for &(b, c) in bc.iter() {
-            let cc_b = *coordinate.get(&b).unwrap();
+            let &cc_b = cc.get(&b).unwrap();
 
             tree.set(cc_b, c.min(tree.get(cc_b)));
         }
