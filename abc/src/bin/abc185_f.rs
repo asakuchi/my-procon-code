e964357b4270caa71d04fd6ae25e67b738fa2d90
asakuchi <paleapricot.asakuchi@gmail.com>
2023-07-17 23:12:37 +0900

    ac-library

--- abc/src/bin/abc185_f.rs
+++ abc/src/bin/abc185_f.rs
@@ -1,80 +1,69 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
+use ac_library_rs::{LazySegtree, MapMonoid, Monoid};
+
+struct Xor;
+
+impl Monoid for Xor {
+    type S = usize;
+
+    fn identity() -> Self::S {
+        0
+    }
+
+    fn binary_operation(&a: &Self::S, &b: &Self::S) -> Self::S {
+        a ^ b
+    }
+}
+
+struct XorAdd;
+
+impl MapMonoid for XorAdd {
+    type M = Xor;
+    type F = usize;
+
+    fn identity_map() -> Self::F {
+        0
+    }
+
+    fn mapping(&f: &Self::F, &x: &<Self::M as Monoid>::S) -> <Self::M as Monoid>::S {
+        x ^ f
+    }
+
+    fn composition(&f: &Self::F, &g: &Self::F) -> Self::F {
+        f ^ g
+    }
+}
+
+#[proconio::fastout]
 fn main() {
     input! {
-        n :usize,
+        n: usize,
         q: usize,
         a: [usize; n],
-        t_x_y: [(usize, usize, usize); q]
     }
 
-    let mut tree = SegmentTreeRmq::new(n);
+    let mut tree = LazySegtree::<XorAdd>::from(a);
 
-    for i in 0..n {
-        tree.update(i, a[i] as isize);
-    }
+    for _ in 0..q {
+        input! {
+            t: usize,
+        }
 
-    for (t, x, y) in t_x_y {
         if t == 1 {
-            let value = tree.query(x - 1, x);
+            input! {
+                x: Usize1,
+                y: usize,
+            }
 
-            tree.update(x - 1, value ^ y as isize);
+            tree.apply(x, y);
         } else {
-            println!("{}", tree.query(x - 1, y));
+            input! {
+                x: Usize1,
+                y: Usize1,
+            }
+
+            println!("{}", tree.prod(x, y + 1));
         }
-    }
-}
-
-struct SegmentTreeRmq {
-    n: usize,
-    data: Vec<isize>,
-}
-
-impl SegmentTreeRmq {
-    fn new(size: usize) -> SegmentTreeRmq {
-        let mut n = 1;
-
-        while n < size {
-            n *= 2;
-        }
-
-        let mut data = vec![0; 2 * n - 1];
-
-        for i in 0..2 * n - 1 {
-            data[i] = 0;
-        }
-
-        SegmentTreeRmq { n, data }
-    }
-
-    fn update(&mut self, k: usize, a: isize) {
-        let mut i = k + self.n - 1;
-        self.data[i] = a;
-
-        while i > 0 {
-            i = (i - 1) / 2;
-            self.data[i] = self.data[i * 2 + 1] ^ self.data[i * 2 + 2];
-        }
-    }
-
-    ///
-    /// [a, b) のxorを求める
-    ///
-    fn query(&self, a: usize, b: usize) -> isize {
-        self.query_inner(a, b, 0, 0, self.n)
-    }
-
-    fn query_inner(&self, a: usize, b: usize, k: usize, l: usize, r: usize) -> isize {
-        if r <= a || b <= l {
-            return 0;
-        }
-
-        if a <= l && r <= b {
-            return self.data[k];
-        }
-        let v_l = self.query_inner(a, b, k * 2 + 1, l, (l + r) / 2);
-        let v_r = self.query_inner(a, b, k * 2 + 2, (l + r) / 2, r);
-
-        v_l ^ v_r
     }
 }
